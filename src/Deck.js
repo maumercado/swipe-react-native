e3934f9259c74a1e83027e8b11497eb435d0351e
Mauricio Mercado <mau@maumercado.com>
2018-01-26 19:31:15 -0800

    First step to using panresponder to control users gestures

--- src/Deck.js
+++ src/Deck.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
-import { View, Animated } from "react-native";
+import { View, Animated, PanResponder } from "react-native";
 
 class Deck extends Component {
+    constructor(props) {
+        super(props);
+        this.position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                this.position.setValue({ x: gesture.dx, y: gesture.dy });
+            },
+            onPanResponderRelease: () => {}
+        });
+
+        this.panResponder = panResponder;
+    }
+
     renderCards() {
         return this.props.data.map(item => {
             return this.props.renderCard(item);
         });
     }
     render() {
-        return <View>{this.renderCards()}</View>;
+        return (
+            <Animated.View style={this.position.getLayout()} {...this.panResponder.panHandlers}>
+                {this.renderCards()}
+            </Animated.View>
+        );
     }
 }
 
