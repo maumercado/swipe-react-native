d2458e4f7a5f7f5f3af8aef70c9edacfd8718a71
Mauricio Mercado <mau@maumercado.com>
2018-01-28 18:48:01 -0800

    Add default props to deck onSwipeXXX

--- src/Deck.js
+++ src/Deck.js
@@ -6,6 +6,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+    };
+
     constructor(props) {
         super(props);
 
@@ -28,11 +33,13 @@ class Deck extends Component {
         });
 
         this.panResponder = panResponder;
+        this.state = { index: 0 };
     }
 
     onSwipeComplete(direction) {
-        const { onSwipeLeft, onSwipeRight } = this.props;
-        direction === "right" ? onSwipeRight() : onSwipeLeft();
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index];
+        direction === "right" ? onSwipeRight(item) : onSwipeLeft(item);
     }
 
     forceSwipe(direction) {
